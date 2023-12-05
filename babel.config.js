5fa16ee7337315b0a7343f7f0fa9984b0f1f2d20
Desplay Shido <desplayshido@gmail.com>
2023-12-05 14:17:50 +0700

    update source

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      ["react-native-reanimated/plugin"],
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@components": "./src/components",
+            "@features": "./src/features",
+            "@data": "./src/data",
+            "@assets": "./assets",
+            "@utils": "./src/utils",
+            "@screens": "./src/screens",
+            "@navigations": "./src/navigations",
+          },
+        },
+      ],
+    ],
+  };
+};
