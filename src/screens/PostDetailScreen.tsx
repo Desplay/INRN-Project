32ef4d80348fcf405c909b2fde3ccbc714e727d2
Desplay Shido <desplayshido@gmail.com>
2023-12-11 17:23:55 +0700

    Create PostDetails Screen

---
+++ src/screens/PostDetailScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PostDetailScreen = () => {
+  return (
+    <View>
+      <Text>PostDetailScreen</Text>
+    </View>
+  )
+}
+
+export default PostDetailScreen
