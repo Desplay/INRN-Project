41ada7b2669310c25319851728f12810e50642de
Desplay Shido <desplayshido@gmail.com>
2023-12-11 17:23:44 +0700

    Create NewPostScreen

---
+++ src/screens/NewPostScreen.tsx
@@ -0,0 +1,20 @@
+import AddNewPost from '@components/newPost/AddNewPost';
+import { View, Text, SafeAreaView, StyleSheet, StatusBar } from 'react-native'
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingTop: StatusBar.currentHeight,
+		backgroundColor: '#181818',
+	},
+});
+
+const NewPostScreen = ({ navigation }: { navigation: any }) => {
+	return (
+		<SafeAreaView style={styles.container}>
+			<AddNewPost navigation={navigation} />
+		</SafeAreaView>
+	)
+}
+
+export default NewPostScreen
