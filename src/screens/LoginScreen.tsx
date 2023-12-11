ff5b2da6ce581abd0840c58f172b3093f963f7dc
Desplay Shido <desplayshido@gmail.com>
2023-12-11 17:23:36 +0700

    Create Login Screen

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,43 @@
+import LoginForm from '@components/login/LoginForm'
+import { useIsFocused } from '@react-navigation/native'
+import { useEffect } from 'react'
+import { View, StyleSheet, Image } from 'react-native'
+import { useSelector } from 'react-redux'
+
+const LoginScreen = ({ navigation }: { navigation: any }) => {
+
+  const token = useSelector((state: any) => state.token).token
+  const focused = useIsFocused()
+
+
+  useEffect(() => {
+    if (focused && token) {
+      console.log(token)
+      navigation.navigate('HomeScreen')
+    }
+  }, [token])
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={require('@assets/logo/Logo.png')} resizeMode='contain' style={{ height: 150, width: 150 }} />
+      </View>
+      <LoginForm navigation={navigation} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#181818',
+    paddingTop: 50,
+    paddingHorizontal: 20,
+  },
+  imageContainer: {
+    alignItems: 'center',
+    marginTop: 50,
+  }
+})
+
+export default LoginScreen
